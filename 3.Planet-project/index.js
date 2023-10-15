d56b9682bf85b82923eb971f80ae3dfec4b433c2
SultanMehmood666 <sultanmehmood9009@gmail.com>
2023-10-15 14:56:30 +0500

    parse data

---
+++ 3.Planet-project/index.js
@@ -0,0 +1,23 @@
+const {parse} = require('csv-parse');
+const fs =  require("fs");
+const result = [];
+const datas = []; 
+
+
+fs.createReadStream('keppler-data.csv')
+//    pipe function act as pipe b/w input-parse & parse 
+    .pipe(parse({
+    comment: '#',
+    columns: true,
+   }))
+//    event
+  .on('data',(data)=>{
+    result.push(data)
+  })
+  .on('end',()=>{
+    console.log(result)
+    console.log('done')
+  })
+  .on('error',(err)=>{
+    console.log(err)
+  })
